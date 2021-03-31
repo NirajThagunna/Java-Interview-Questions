fd9cd2c68999e84d516c2df0a5ca2e5d10e91f2d
NirajThagunna <nirajthagunna123@gmail.com>
2021-03-31 18:53:51 +0545

    Class name related problem

---
+++ src/switchStatements/temp.java
@@ -0,0 +1,45 @@
+package switchStatements;
+
+/*
+ * @Author : Niraj Thagunna
+ * Problems on the switch statements
+ * Java Frequently Asked Interview Questions on Switch Statements -4
+ */
+public class temp { // It's not mandatory or compulsory to have a class name starts with Capital Letter
+
+	public static void main(String[] args) {
+		
+		int ok = 10;
+		switch(ok) {
+		
+		default:
+			System.out.println("default");
+		case 0:
+			System.out.println("true");
+		case 1:
+			System.out.println("false");
+		}
+	}
+}
+
+
+/*
+ * Output : default
+ * true
+ * false
+ * 
+ * You can declare a class name with lower case, but the convention is to start with a capital letter. 
+ * The conventions were created to make it easier on others to read and understand your code.
+ * By following conventions, others (including us and your instructors, bosses and co-workers) can better understand and 
+ * evaluate your code.
+ */
+
+
+
+
+
+
+
+
+
+
